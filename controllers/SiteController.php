039d3a49c1fa80303731079331a260fb15204ca8
ArtemKydr <337755@niuitmo.ru>
2022-12-30 16:26:06 +0300

    test2

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -91,7 +91,7 @@ class SiteController extends Controller
     public function actionLogout()
     {
         Yii::$app->user->logout();
-
+        $USERNAME = 123;
         return $this->goHome();
     }
 
