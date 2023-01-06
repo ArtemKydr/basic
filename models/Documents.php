855b16521a621118dd4d4aa4be8a809e84f9bb14
ArtemKydr <337755@niuitmo.ru>
2023-01-06 18:17:42 +0300

    Сделана форма в личном кабинете студента Сделана таблица в личном кабинете студента

---
+++ models/Documents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\models;
+use yii\db\ActiveRecord;
+class Documents extends ActiveRecord
+{
+    public function rules()
+    {
+        return [];
+    }
+}
---
+++ models/UploadDocumentForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+class UploadDocumentForm extends Model
+{
+/**
+* @var UploadedFile
+*/
+    public $file;
+    public $authors;
+    public $university;
+    public $name;
+    public $nr;
+    public $title;
+    public $fio;
+    public $organization;
+    public $city;
+    public $email;
+    public $phone;
+
+    public function rules()
+    {
+        return [
+            [['title','fio','organization','city','email','phone'],'required','message'=>'*']
+        ];
+    }
+
+    public function upload()
+    {
+            $name = strtolower($this->file->baseName);
+            $this->file->saveAs('UploadDocument/' . $this->file->baseName . '.' . $this->file->extension);
+            return true;
+    }
+    public function attributeLabels()
+    {
+        return [
+            'email' => 'Электронная почта',
+            'fio'=>'ФИО',
+            'phone'=>'Телефон',
+            'city'=>'Город',
+            'organization'=>'Организация',
+            'nr'=>'Научный руководитель',
+            'title'=>'Название статьи',
+            'authors'=>'Автор',
+            'university'=>'ВУЗ',
+            'file'=>'Статья'
+
+        ];
+    }
+}
