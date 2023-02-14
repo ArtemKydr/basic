17b9f8d6e716552e9f3f3cff3fdce7b0c6378a0e
ArtemKydr <337755@niuitmo.ru>
2023-02-14 22:44:50 +0300

    prod

--- web/index.php
+++ web/index.php
@@ -1,10 +1,10 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-//define('YII_ENABLE_ERROR_HANDLER', false);
-//define('YII_ENABLE_EXCEPTION_HANDLER', false);
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_ENABLE_EXCEPTION_HANDLER', false);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
