f11fcc1540a9af7d7dbdcae70dbcab23a89b1b3a
ArtemKydr <337755@niuitmo.ru>
2023-02-15 11:23:43 +0300

    правки по загрузке доп доков

--- models/AdditionalFiles.php
+++ models/AdditionalFiles.php
@@ -28,7 +28,44 @@ class AdditionalFiles extends ActiveRecord
 
     public function upload()
     {
+        $timestamp = date('dmYHis');
+        $expert = $_FILES['AdditionalFiles']['name']['expert'];
+        $expert = mb_strtolower(UploadDocumentForm::transliterate($expert));
+        $review = $_FILES['AdditionalFiles']['name']['review'];
+        $review =  mb_strtolower(UploadDocumentForm::transliterate($review));
+        $file_scan = $_FILES['AdditionalFiles']['name']['file_scan'];
+        $file_scan =  mb_strtolower(UploadDocumentForm::transliterate($file_scan));
         if ($this->validate()) {
+            if ($expert!='' and $review!='' and $file_scan!='')
+            {
+                $this->expert->saveAs('UploadDocumentExpert/'.$timestamp.'_'. $expert,false);
+                $this->file_scan->saveAs('UploadDocumentFileScan/'.$timestamp.'_'. $file_scan,false);
+                $this->review->saveAs('UploadDocumentReview/'.$timestamp.'_'. $review,false);
+            }else if (($expert!='' or $review!='') and $file_scan==''){
+                if ($expert!=''){
+                    $this->expert->saveAs('UploadDocumentExpert/'.$timestamp.'_'. $expert,false);
+                }
+                if ($review!=''){
+                    $this->review->saveAs('UploadDocumentReview/'.$timestamp.'_'. $review,false);
+                }
+            }
+            else if (($expert!=''or $file_scan!='') and $review==''){
+                if ($file_scan!=''){
+                    $this->file_scan->saveAs('UploadDocumentFileScan/'.$timestamp.'_'. $file_scan,false);
+                }
+                if ($expert!=''){
+                    $this->expert->saveAs('UploadDocumentExpert/'.$timestamp.'_'. $expert,false);
+                }
+            }
+            else if (($review!=''or $file_scan!='') and $expert==''){
+                if ($file_scan!=''){
+                    $this->file_scan->saveAs('UploadDocumentFileScan/'.$timestamp.'_'. $file_scan,false);
+
+                }
+                if ($review!=''){
+                    $this->review->saveAs('UploadDocumentReview/'.$timestamp.'_'. $review,false);
+                }
+            }
             return true;
         } else {
             return false;
